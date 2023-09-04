89a631b44ed2d16160feda703977e5db4a404ece
agboola <agboolanathaniel@yahoo.com>
2023-09-04 10:38:12 +0100

    camera permission

--- utils/location.js
+++ utils/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = 'AIzaSyBUC8iVa4qB2Ft2_gmA2ahTa9eoyJC2C6M';
+
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap
